3a85a1cd87f39668905c27d05329c06af9eccab2
NshanPotikyan <nshan.potikyan@gmail.com>
2023-04-02 13:35:59 +0400

    Add midterm files.

--- utils/kahoot.py
+++ utils/kahoot.py
@@ -15,7 +15,7 @@ class KahootParser:
         self.files = um.get_files(path=path_to_folder, file_type='xlsx')
         self.correct_name_mapping = {}
 
-    def parse_one_kahoot(self, one_kahoot):
+    def parse_per_student(self, one_kahoot):
         """
         Parses one kahoot report by leaving relevant information and assigns names to students.
 
@@ -25,14 +25,12 @@ class KahootParser:
         """
         data = pd.read_excel(one_kahoot, sheet_name="RawReportData Data")
         data = data[["Question Number", "Player",
-                     "Correct", "Answer Time (seconds)",
-                     "Time Allotted to Answer (seconds)"]]
+                     "Correct", "Answer Time (seconds)"]]
         data.rename(columns={
             'Question Number': 'question_id',
             'Player': 'student',
             'Correct': 'correct',
             'Answer Time (seconds)': 'time',
-            'Time Allotted to Answer (seconds)': 'total_time'
         }, inplace=True)
         data.question_id = data.question_id.str.replace(' Quiz', '').astype(int)
 
@@ -62,12 +60,16 @@ class KahootParser:
         data.student_id = data.student_id.map(self.student_ids)
         return data
 
-    def process_kahoot_xlsxs(self):
+    def process_kahoot_xlsxs(self, per_student=True):
         """
         Goes over all kahoot reports and generates csv files by leaving relevant information.
         """
         for file in self.files:
-            self.parse_one_kahoot(file).to_csv(file.replace('xlsx', 'csv'), index=False)
+            if per_student:
+                self.parse_per_student(file).to_csv(file.replace('xlsx', 'csv'), index=False)
+            else:
+                # per question
+                self.parse_per_question(file).to_csv(file.replace('xlsx', 'csv'), index=False)
 
     def replace_names_with_ids(self):
         """
@@ -77,8 +79,27 @@ class KahootParser:
             csv_name = file.replace('xlsx', 'csv')
             self.parse_one_csv(csv_name).to_csv(csv_name.replace(' ', ''), index=False)
 
+    @staticmethod
+    def parse_per_question(one_kahoot):
+        data = pd.read_excel(one_kahoot, sheet_name="RawReportData Data")
+        data = data[["Question Number", "Question",
+                     "Answer 1", "Answer 2", "Answer 3", "Answer 4",
+                     "Time Allotted to Answer (seconds)"]]
+        data.rename(columns={
+            'Question Number': 'question_id',
+            "Question": 'question',
+            "Answer 1": 'answer1',
+            "Answer 2": 'answer2',
+            "Answer 3": 'answer3',
+            "Answer 4": 'answer4',
+            'Time Allotted to Answer (seconds)': 'total_time'
+        }, inplace=True)
+        data.question_id = data.question_id.str.replace(' Quiz', '').astype(int)
+        data.drop_duplicates(inplace=True)
+        return data
+
 
 if __name__ == '__main__':
-    kahoot_parser = KahootParser(path_to_folder='kahoots',
-                                 student_ids='students.csv')
-    kahoot_parser.replace_names_with_ids()
+    kahoot_parser = KahootParser(path_to_folder='/home/nshan/Documents/TeachingPython/Kahoot/reports_asds',
+                                 student_ids='../asds/students.csv')
+    kahoot_parser.process_kahoot_xlsxs(per_student=False)
